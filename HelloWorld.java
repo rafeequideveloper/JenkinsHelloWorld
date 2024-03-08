01dad0bef24d0c226fba9c74d1e07a915dd91493
rafeequideveloper <85687197+rafeequideveloper@users.noreply.github.com>
2024-03-08 18:15:37 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World & Welcome to jenkings trainings");
+        System.out.println("Made Some Changes");
     }
 }
